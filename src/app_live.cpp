c73a29d65cce681e105fa527f49669a2404b0430
raeaw <raeaw@nyanpad>
2022-02-09 21:13:39 +1030

    fill out app::Live::load and app::Live::save

--- src/app_live.cpp
+++ src/app_live.cpp
@@ -4,11 +4,12 @@ using namespace app;
 
 void Live::check_for_new_capture(const std::string& filename)
 {
-	if (!utils::check_file_exists(filename)) {
+	auto path = std::filesystem::path(filename);
+	if (std::filesystem::exists(path)) {
 		return;
 	}
 
-	std::cout << "loading " << filename << "..." << std::endl;
+	std::cout << "loading " << path << "..." << std::endl;
 	auto img = NamedImg{
 		filename,
 		cv::imread(filename, cv::IMREAD_COLOR)
@@ -18,14 +19,40 @@ void Live::check_for_new_capture(const std::string& filename)
 	averager.push(img.img, face);
 
 	save();
+
+	std::filesystem::remove(path);
 }
 
-void Live::set_output_folder(std::string output_folder)
+void Live::set_output_folder(const std::string& output_folder)
 {
-	if (output_folder.back() != '/') {
-		output_folder += std::string("/");
-	}
-	output_img_filename = output_folder + std::string("avg.jpg");
-	output_face_filename = output_folder + std::string("avg.face");
-	num_faces_filename = output_folder + std::string("num_faces.u64");
+	auto output_path = std::filesystem::path(output_folder);
+	output_img_path = output_path / "avg.jpg";
+	output_face_path = output_path / "avg.face";
+	num_faces_path = output_path / "num_faces.u64";
+}
+
+void Live::save()
+{
+	auto avg_img = averager.get_avg_img();
+	auto avg_face = averager.get_avg_face();
+	auto num_faces = averager.get_num_faces();
+
+	utils::save_num_faces(num_faces_path, num_faces);
+	avg_face.save(output_face_path);
+	cv::imwrite(output_img_path, avg_img);
+}
+
+void Live::load(const std::string& folder)
+{
+	auto output_path = std::filesystem::path(folder);
+	auto avg_img_path = output_path / "avg.jpg";
+	auto avg_face_path = output_path / "avg.face";
+	auto backup_num_faces_path = output_path / "num_faces.u64";
+
+	auto num_faces = utils::load_num_faces(backup_num_faces_path);
+	auto avg_face = face::Face::load(avg_face_path);
+	avg_face.set_name(avg_face_path);
+	auto avg_img = cv::imread(avg_img_path, cv::IMREAD_COLOR);
+
+	averager.set(avg_img, avg_face, num_faces);
 }
--- src/app_live.hpp
+++ src/app_live.hpp
@@ -2,6 +2,7 @@
 #define APP_LIVE_HPP_
 
 #include <fstream>
+#include <filesystem>
 #include "face_averager.hpp"
 
 namespace app
@@ -10,19 +11,18 @@ namespace app
 class Live {
 	face::FaceAverager averager;
 	face::FaceDetector detector;
-	u64 num_faces;
 
-	std::string output_img_filename;
-	std::string output_face_filename;
-	std::string num_faces_filename;
+	std::filesystem::path output_img_path;
+	std::filesystem::path output_face_path;
+	std::filesystem::path num_faces_path;
 
 public:
 	void check_for_new_capture(const std::string& filename);
 
-	void set_output_folder(std::string output_folder);
+	void set_output_folder(const std::string& output_folder);
 
 	void save();
-	void load();
+	void load(const std::string& folder);
 };
 
 }

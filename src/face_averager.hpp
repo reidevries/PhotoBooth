80ada3a98bedee45c8c774009ccd956fc8461e84
raeaw <raeaw@nyanpad>
2022-02-09 20:35:11 +1030

    add getters to FaceAverager

--- src/face_averager.hpp
+++ src/face_averager.hpp
@@ -10,10 +10,13 @@ class FaceAverager
 {
 	cv::Mat avg_img;
 	Face avg_face;
-	int num_faces;
+	u64 num_faces;
 	float param = 1.0;
 public:
 	void set_param(float param);
+	auto get_avg_img() -> cv::Mat { return avg_img; }
+	auto get_avg_face() -> Face { return avg_face; }
+	auto get_num_faces() -> u64 { return num_faces; }
 	auto push(const cv::Mat& img, const Face& face) -> cv::Mat;
 	auto process(
 		const cv::Mat& img,

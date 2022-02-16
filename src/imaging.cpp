b34b69e0e8695e3f51007acf8e87a7b1b9e0cf67
raeaw <raeaw@nyanpad>
2022-02-16 20:45:23 +1030

    let program draw lines on processed image

--- src/imaging.cpp
+++ src/imaging.cpp
@@ -24,7 +24,43 @@ auto imaging::load_img_and_process(
 
 auto imaging::process_aly_style(const cv::Mat& img) -> cv::Mat
 {
-	auto out = cv::Mat();
-	cv::repeat(img, 2, 2, out);
+	auto proc = img;
+
+	static const cv::Scalar WHITE = cv::Scalar(255, 255, 255);
+
+	auto size = img.size();
+	static const int d = 10;
+	static const int w = 40;
+	static const cv::LineTypes l_t = cv::LINE_AA;
+	auto l_w = size.height/w;
+	auto x_d = size.width/d;
+	auto y_d = size.height/d;
+
+	auto p_tl = cv::Point(x_d, y_d);
+	auto p_tl_b = p_tl + cv::Point(0, y_d);
+	auto p_tl_r = p_tl + cv::Point(x_d, 0);
+	cv::line(proc, p_tl, p_tl_b, WHITE, l_w, l_t);
+	cv::line(proc, p_tl, p_tl_r, WHITE, l_w, l_t);
+
+	auto p_tr = cv::Point((d-1)*x_d, y_d);
+	auto p_tr_b = p_tr + cv::Point(0, y_d);
+	auto p_tr_l = p_tr - cv::Point(x_d, 0);
+	cv::line(proc, p_tr, p_tr_b, WHITE, l_w, l_t);
+	cv::line(proc, p_tr, p_tr_l, WHITE, l_w, l_t);
+
+	auto p_bl = cv::Point(x_d, (d-1)*y_d);
+	auto p_bl_t = p_bl - cv::Point(0, y_d);
+	auto p_bl_r = p_bl + cv::Point(x_d, 0);
+	cv::line(proc, p_bl, p_bl_t, WHITE, l_w, l_t);
+	cv::line(proc, p_bl, p_bl_r, WHITE, l_w, l_t);
+
+	auto p_br = cv::Point((d-1)*x_d, (d-1)*y_d);
+	auto p_br_t = p_br - cv::Point(0, y_d);
+	auto p_br_l = p_br - cv::Point(x_d, 0);
+	cv::line(proc, p_br, p_br_t, WHITE, l_w, l_t);
+	cv::line(proc, p_br, p_br_l, WHITE, l_w, l_t);
+
+	auto out = proc;
+	cv::repeat(proc, 2, 2, out);
 	return out;
 }

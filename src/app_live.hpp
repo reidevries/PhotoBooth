f602c353cb48eb89e015969dd49bd1ccab6f82fd
raeaw <raeaw@nyanpad>
2022-03-18 17:58:21 +1030

    document app_live.hpp

--- src/app_live.hpp
+++ src/app_live.hpp
@@ -10,11 +10,17 @@ namespace app
 {
 
 class LiveProcess {
+	/// the face averager, handles face averaging
 	face::FaceAverager averager;
+	/// the face detector, used as part of face averager
 	face::FaceDetector detector;
 
+	/// stores the output paths of each component required to save/load the
+	/// state of `averager`
 	face::OutputPaths save_paths;
+	/// stores the last clock time a capture was found
 	std::filesystem::file_time_type last_write_time;
+	/// stores the system call used to print
 	std::string print_cmd;
 public:
 	LiveProcess();
@@ -24,10 +30,25 @@ public:
 	);
 
 	/**
-	 * check for a new
+	 * First, check if a new capture image exists. If it does, and it's more
+	 * recent than the last capture image that was processed, then load it and
+	 * push it to the average.
+	 * Second, save the newly processed image, as well as the current average,
+	 * in a folder that can be set with `set_save_paths`. 
+	 * Third, print it using a system call to the linux utility `lp`.
+	 * Finally, delete the processed capture.
+	 * @param filename The filename to look for the new capture at
 	 */
 	void try_process_new_capture(const std::string& filename);
+
+	/**
+	 * sets `save_paths` using the given folder.
+	 */
 	void set_save_paths(const std::string& folder);
+
+	/**
+	 * Loads the average stored in `save_paths`
+	 */
 	void load_avg();
 };
 

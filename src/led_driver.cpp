fd59ee8062126cef6b0f324054042d6942d9bc85
raeaw <raeaw@nyanpad>
2022-07-03 19:56:33 +0930

    create multithreaded pulse function

--- src/led_driver.cpp
+++ src/led_driver.cpp
@@ -1,5 +1,21 @@
 #include "led_driver.hpp"
 
+void* LedDriver::pulse(void* user_data)
+{
+	auto args = reinterpret_cast<PulseArgs*>(user_data);
+	while (1) 
+	{
+		gpioWrite(args->gpio, 1);
+		std::this_thread::sleep_for(std::chrono::milliseconds(
+				static_cast<int>(1000*args->pulsewidth_s)
+		));
+		gpioWrite(args->gpio, 0);
+		std::this_thread::sleep_for(std::chrono::milliseconds(
+				static_cast<int>(1000*args->spacing_s)
+		));
+	}
+}
+
 void LedDriver::init(int gpio)
 {
 	this->gpio = gpio;
@@ -18,4 +34,18 @@ void LedDriver::countdown(float spacing_s, float pulsewidth_s, int num_pulses)
 			std::chrono::milliseconds(static_cast<int>(1000*spacing_s))
 		);
 	}
+}
+
+pthread_t* LedDriver::start_pulse(float spacing_s, float pulsewidth_s)
+{
+	auto pulse_args = new PulseArgs();
+	pulse_args->gpio = gpio;
+	pulse_args->spacing_s = spacing_s;
+	pulse_args->pulsewidth_s = pulsewidth_s;
+	return gpioStartThread(LedDriver::pulse, pulse_args);
+}
+
+void stop_pulse(pthread_t* thread_ptr)
+{
+	gpioStopThread(thread_ptr);
 }
--- src/led_driver.hpp
+++ src/led_driver.hpp
@@ -6,15 +6,27 @@
 #include <chrono>
 #include <thread>
 
+
+
 class LedDriver
 {
 private:
 	int gpio;
-	
+
+	struct PulseArgs
+	{
+		int gpio;
+		float spacing_s;
+		float pulsewidth_s;
+	};
+	static void* pulse(void *arg);
 public:
 	void init(int gpio);
 
 	void countdown(float spacing_s, float pulsewidth_s, int num_pulses);
+
+	pthread_t* start_pulse(float spacing_s, float pulsewidth_s);
+	void stop_pulse(pthread_t* thread_ptr);
 };
 
 #endif
